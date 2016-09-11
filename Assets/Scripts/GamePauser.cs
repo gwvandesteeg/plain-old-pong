e83e70ee09b2067d31560c15a1c0d9e42b976c88
Gerwin van de Steeg <gerwin@vandesteeg.co.nz>
2016-09-11 17:56:08 +1200

    Add ability to Pause game using the Pause button

---
+++ Assets/Scripts/GamePauser.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * Copyright (c) 2016, Gerwin van de Steeg
+ * All rights reserved.
+ *
+ * This program is free software, distributed under the
+ * BSD 2 Clause license, see the LICENSE file at the
+ * top of the source tree for a full copy of the license.
+ * 
+ */
+using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections;
+
+namespace PaddleGame {
+
+	/**
+	 * The provides a means to pause the game
+	 * 
+	 * @author	Gerwin van de Steeg
+	 *
+	 */
+	public class GamePauser : MonoBehaviour, IUserInput {
+		//! the Input proxy
+		public IUserInputProvider InputProvider { get; set;}
+		//! store the old timescale
+		private float oldTimeScale;
+
+		// Update is called once per frame
+		void Update () {
+			Debug.Log ("Pause: " + KeyCode.Pause);
+			// check i the input provider has been configured
+			if (InputProvider == null) {
+				Debug.Log ("No input provider set, nothing to do");
+			} else {
+				// if the Pause key has been pressed
+				if (InputProvider.GetKeyDown (KeyCode.Pause)) {
+					// toggle game time scale
+					if (Time.timeScale > 0f) {
+						oldTimeScale = Time.timeScale;
+						Debug.Log("Pausing game, storing old time scale: " + oldTimeScale.ToString());
+						Time.timeScale = 0f;
+					} else {
+						Time.timeScale = oldTimeScale;
+						Debug.Log("Unpausing game, restoring time scale to: " + oldTimeScale);
+					}
+				}
+			}
+		}
+	}
+}
